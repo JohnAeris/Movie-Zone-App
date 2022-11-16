62abeb73d719e741b5961d70019d4a07bb484a0c
johnaeris <barrotjohnaeris@gmail.com>
2022-11-16 14:32:05 +0800

    Add top app bar

--- app/src/main/java/com/example/moviezone/MainActivity.kt
+++ app/src/main/java/com/example/moviezone/MainActivity.kt
@@ -1,43 +1,95 @@
 package com.example.moviezone
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.moviezone.ui.theme.MovieZoneTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MovieZoneTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+            MyApp {
+                MainContent()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyApp(content: @Composable () -> Unit) {
+    MovieZoneTheme {
+        Scaffold(
+            topBar = {
+                TopAppBar(
+                    backgroundColor = MaterialTheme.colors.surface,
+                    elevation = 4.dp
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier
+                            .padding(start = 10.dp))
+                    {
+
+                        Image(
+                            painter = painterResource(R.drawable.app_ic),
+                            contentDescription = "Application's Icon",
+                            modifier = Modifier.size(25.dp)
+                        )
+
+                        Spacer(modifier = Modifier.width(10.dp))
+
+
+                        Text(
+                            text = "MovieZone",
+                            style = MaterialTheme.typography.h1,
+                            color = MaterialTheme.colors.primary,
+                            modifier = Modifier.padding(bottom = 8.dp)
+                        )
+                    }
+                }
+            }
+        ) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun MainContent() {
+    Surface() {
+        Column(
+            modifier = Modifier
+                .background(color = MaterialTheme.colors.background)
+                .fillMaxSize())
+        {
+            Text(
+                text = "Hello Worlddd",
+                color = MaterialTheme.colors.primary,
+                style = MaterialTheme.typography.body1
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    MovieZoneTheme {
-        Greeting("Android")
+    MyApp() {
+        MainContent()
     }
 }
