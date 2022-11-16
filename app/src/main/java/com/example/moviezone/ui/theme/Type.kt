a173797c3a9eb4af4ddce75c1ed0f29e9b77e8ca
johnaeris <barrotjohnaeris@gmail.com>
2022-11-16 14:31:34 +0800

    Add font-styles

--- app/src/main/java/com/example/moviezone/ui/theme/Type.kt
+++ app/src/main/java/com/example/moviezone/ui/theme/Type.kt
@@ -1,28 +1,76 @@
 package com.example.moviezone.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.ui.res.fontResource
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.moviezone.R
+
+val Bangers = FontFamily(Font(R.font.bangers_regular))
+
+val PoppinsBold = FontFamily(Font(R.font.poppins_bold))
+val PoppinsSemiBold = FontFamily(Font(R.font.poppins_semi_bold))
+val PoppinsMedium = FontFamily(Font(R.font.poppins_medium))
+val PoppinsNormal = FontFamily(Font(R.font.poppins_regular))
+val PoppinsExtraLight = FontFamily(Font(R.font.poppins_extra_light))
+val PoppinsExtraLightItalic = FontFamily(Font(R.font.poppins_extra_light_italic))
+val PoppinsThin = FontFamily(Font(R.font.poppins_thin))
+val PoppinsThinItalic = FontFamily(Font(R.font.poppins_thin))
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
-        fontSize = 14.sp
+
+    h1 = TextStyle(
+        fontFamily = Bangers,
+        fontSize = 24.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+
+    h2 = TextStyle(
+        fontFamily = PoppinsBold,
+        fontSize = 24.sp
+    ),
+
+    h3 = TextStyle(
+        fontFamily = PoppinsSemiBold,
+        fontSize = 20.sp
+    ),
+
+    h4 = TextStyle(
+        fontFamily = PoppinsMedium,
+        fontSize = 16.sp
+    ),
+
+    h5 = TextStyle(
+        fontFamily = PoppinsNormal,
+        fontSize = 16.sp
+    ),
+
+    h6 = TextStyle(
+        fontFamily = PoppinsExtraLight,
+        fontSize = 16.sp
+    ),
+
+    body1 = TextStyle(
+        fontFamily = PoppinsNormal,
+        fontSize = 12.sp
+    ),
+
+    body2 = TextStyle(
+        fontFamily = PoppinsExtraLightItalic,
+        fontSize = 12.sp
+    ),
+
+    subtitle1 = TextStyle(
+        fontFamily = PoppinsThin,
+        fontSize = 12.sp
+    ),
+
+    subtitle2 = TextStyle(
+        fontFamily = PoppinsThinItalic,
         fontSize = 12.sp
     )
-    */
+
 )
