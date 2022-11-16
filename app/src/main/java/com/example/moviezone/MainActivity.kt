0bee7d3a666586481ba3734800774119856e59b7
johnaeris <barrotjohnaeris@gmail.com>
2022-11-16 16:37:46 +0800

    Add lazyrow movie posters

--- app/src/main/java/com/example/moviezone/MainActivity.kt
+++ app/src/main/java/com/example/moviezone/MainActivity.kt
@@ -7,12 +7,21 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -70,19 +79,64 @@ fun MyApp(content: @Composable () -> Unit) {
 }
 
 @Composable
-fun MainContent() {
-    Surface() {
-        Column(
-            modifier = Modifier
-                .background(color = MaterialTheme.colors.background)
-                .fillMaxSize())
-        {
+fun MainContent(moviePosterList: List<Painter> =
+    listOf(
+        painterResource(id = R.drawable.jujutsu_poster),
+        painterResource(id = R.drawable.bluelock_poster),
+        painterResource(id = R.drawable.hunter_poster),
+        painterResource(id = R.drawable.bleach_poster),
+        painterResource(id = R.drawable.demon_slayer)
+    )
+) {
+    Column(
+        modifier = Modifier.padding(20.dp),
+        verticalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
+
+        Column() {
             Text(
-                text = "Hello Worlddd",
-                color = MaterialTheme.colors.primary,
-                style = MaterialTheme.typography.body1
-            )
+                text = "Movies",
+                style = MaterialTheme.typography.h2,
+                color = MaterialTheme.colors.primary)
+
+            Spacer(modifier = Modifier.height(5.dp))
+
+            Text(
+                text = stringResource(R.string.movie_description),
+                style = MaterialTheme.typography.body1,
+                color = MaterialTheme.colors.primary)
         }
+
+        Column() {
+            Text(
+                text = "Anime",
+                style = MaterialTheme.typography.h3,
+                color = MaterialTheme.colors.primary)
+
+            LazyRow() {
+                items(items = moviePosterList) { item ->
+                    MovieCard(item)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun MovieCard(item: Painter) {
+    Card(
+        modifier = Modifier
+            .wrapContentSize()
+            .padding(start = 5.dp, end = 5.dp)
+            .size(100.dp, 140.dp),
+        shape = RoundedCornerShape(corner = CornerSize(4.dp)),
+        elevation = 8.dp)
+    {
+        Image(
+            painter = item,
+            contentDescription = "Movie Poster",
+            contentScale = ContentScale.FillBounds
+        )
     }
 }
 
