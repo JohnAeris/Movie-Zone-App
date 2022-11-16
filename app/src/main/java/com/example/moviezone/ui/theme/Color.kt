2090d83706afe0b1f925f200800a2cc854b07068
johnaeris <barrotjohnaeris@gmail.com>
2022-11-16 14:30:49 +0800

    Add color palette

--- app/src/main/java/com/example/moviezone/ui/theme/Color.kt
+++ app/src/main/java/com/example/moviezone/ui/theme/Color.kt
@@ -6,3 +6,13 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+/*My Color Palette*/
+
+val white = Color(0xFFFFFFFF)
+val black = Color(0xFF000000)
+val red_ryb = Color(0xFFFF220C)
+val english_vermillion = Color(0xFFD33E43)
+val burnished_brown = Color(0xFF9B7874)
+val old_lavender = Color(0xFF666370)
+val space_cadet = Color(0xFF1C1F33)
