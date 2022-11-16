4481126f72a0439517991d2cda82312d55b4ccdb
johnaeris <barrotjohnaeris@gmail.com>
2022-11-16 22:46:45 +0800

    Add display id for testing

--- app/src/main/java/com/example/moviezone/screens/DetailScreen.kt
+++ app/src/main/java/com/example/moviezone/screens/DetailScreen.kt
@@ -1,13 +1,51 @@
 package com.example.moviezone.screens
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material.Text
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
-fun DetailScreen(navController: NavController) {
-    Column() {
-        Text(text = "Hello World")
+fun DetailScreen(navController: NavController, movie: String?) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                backgroundColor = MaterialTheme.colors.surface,
+                elevation = 4.dp
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier
+                        .padding(start = 10.dp))
+                {
+                    
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "Arrow Back",
+                        modifier = Modifier
+                            .clickable { navController.popBackStack() },
+                        tint = MaterialTheme.colors.primary
+                    )
+                    
+                    Spacer(modifier = Modifier.width(5.dp))
+
+                    Text(
+                        text = "Back",
+                        style = MaterialTheme.typography.h3,
+                        color = MaterialTheme.colors.primary,
+                        modifier = Modifier.padding(top = 5.dp)
+                    )
+
+                }
+            }
+        }
+    ) {
+        Text(text = movie.toString())
     }
 }
