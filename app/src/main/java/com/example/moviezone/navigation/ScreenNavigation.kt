8b73b65b593e964889f2cde56a356e71902fbab1
johnaeris <barrotjohnaeris@gmail.com>
2022-11-16 22:46:16 +0800

    Add passing arguments

--- app/src/main/java/com/example/moviezone/navigation/ScreenNavigation.kt
+++ app/src/main/java/com/example/moviezone/navigation/ScreenNavigation.kt
@@ -1,9 +1,11 @@
 package com.example.moviezone.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.moviezone.screens.DetailScreen
 import com.example.moviezone.screens.HomeScreen
 
@@ -14,7 +16,11 @@ fun ScreenNavigation() {
     NavHost(navController = navController, startDestination = Screen.HomeScreen.name) {
 
         composable(Screen.HomeScreen.name) { HomeScreen(navController) }
-        composable(Screen.DetailScreen.name) { DetailScreen(navController) }
 
+        composable(Screen.DetailScreen.name + "/{movie}",
+            arguments = listOf(navArgument(name = "movie") {type = NavType.StringType})
+        ) { backStackEntry ->
+            DetailScreen(navController, backStackEntry.arguments?.getString("movie"))
+        }
     }
 }
