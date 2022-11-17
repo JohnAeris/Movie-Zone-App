2fd9bb7a7cc6389ece6134d1693aa919c2c925d3
johnaeris <barrotjohnaeris@gmail.com>
2022-11-17 16:11:28 +0800

    Add movie detail's user interface

--- app/src/main/java/com/example/moviezone/screens/DetailScreen.kt
+++ app/src/main/java/com/example/moviezone/screens/DetailScreen.kt
@@ -1,16 +1,18 @@
 package com.example.moviezone.screens
 
 import android.widget.Toast
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.PlayArrow
-import androidx.compose.runtime.Composable
+import androidx.compose.material.icons.filled.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,10 +20,7 @@ import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.withStyle
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
@@ -29,10 +28,9 @@ import coil.compose.AsyncImagePainter
 import coil.compose.SubcomposeAsyncImage
 import coil.compose.SubcomposeAsyncImageContent
 import com.example.moviezone.R
-import com.example.moviezone.model.MovieData
+import com.example.moviezone.components.FilledRectangleButton
+import com.example.moviezone.components.OutlinedRectangleButton
 import com.example.moviezone.model.getMovieData
-import com.example.moviezone.ui.theme.PoppinsExtraLight
-import com.example.moviezone.ui.theme.PoppinsMedium
 
 @Composable
 fun DetailScreen(navController: NavController, movieId: String?) {
@@ -79,111 +77,228 @@ fun DetailScreen(navController: NavController, movieId: String?) {
         Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
 
             val context = LocalContext.current
+            var isReadMore by remember {
+                mutableStateOf(false)
+            }
 
-            Column {
-                Box(contentAlignment = Alignment.Center) {
-                    Card(
-                        backgroundColor = MaterialTheme.colors.onBackground,
-                        shape = RectangleShape,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .size(200.dp)) {
+            LazyColumn {
 
-                        SubcomposeAsyncImage(
-                            model = movie.image,
-                            contentDescription = "{${movie.title} Image}",
-                            contentScale = ContentScale.FillBounds
-                        ) {
-                            val state = painter.state
-                            if (state is AsyncImagePainter.State.Loading || state is AsyncImagePainter.State.Error) {
-                                CircularProgressIndicator()
-                            } else {
-                                SubcomposeAsyncImageContent()
-                            }
-                        }
-                    }
+                item {
+                     Box(contentAlignment = Alignment.Center) {
+                         Card(
+                             backgroundColor = MaterialTheme.colors.onBackground,
+                             shape = RectangleShape,
+                             modifier = Modifier
+                                 .fillMaxWidth()
+                                 .size(200.dp)) {
 
-                    Card(
-                        shape = CircleShape,
-                        backgroundColor = Color.White,
-                        elevation = 8.dp,
-                        modifier = Modifier
-                            .clickable {
-                                Toast
-                                    .makeText(context, "Play ${movie.title}", Toast.LENGTH_SHORT)
-                                    .show()
-                            }
-                            .size(50.dp),
-                    )
-                    {
-                        Icon(
-                            imageVector = Icons.Default.PlayArrow,
-                            contentDescription = "Play Button",
-                            tint = colorResource(id = R.color.red_ryb),
-                            modifier = Modifier.size(40.dp)
-                        )
+                             SubcomposeAsyncImage(
+                                 model = movie.image,
+                                 contentDescription = "{${movie.title} Image}",
+                                 contentScale = ContentScale.FillBounds
+                             ) {
+                                 val state = painter.state
+                                 if (state is AsyncImagePainter.State.Loading || state is AsyncImagePainter.State.Error) {
+                                     CircularProgressIndicator()
+                                 } else {
+                                     SubcomposeAsyncImageContent()
+                                 }
+                             }
+                         }
+
+                         Card(
+                             shape = CircleShape,
+                             backgroundColor = Color.White,
+                             elevation = 8.dp,
+                             modifier = Modifier
+                                 .clickable {
+                                     Toast
+                                         .makeText(
+                                             context,
+                                             "Play ${movie.title}",
+                                             Toast.LENGTH_SHORT
+                                         )
+                                         .show()
+                                 }
+                                 .size(50.dp),
+                         )
+                         {
+                             Icon(
+                                 imageVector = Icons.Default.PlayArrow,
+                                 contentDescription = "Play Button",
+                                 tint = colorResource(id = R.color.red_ryb),
+                                 modifier = Modifier.size(40.dp)
+                             )
+                         }
+                     }
+                 }
+
+                item {
+                    Box() {
+                        Divider(color = colorResource(id = R.color.old_lavender), thickness = 4.dp)
+                        Divider(color = colorResource(id = R.color.red_ryb), thickness = 4.dp, modifier = Modifier.width(10.dp))
                     }
                 }
 
-                Box() {
-                    Divider(color = colorResource(id = R.color.old_lavender), thickness = 4.dp)
-                    Divider(color = colorResource(id = R.color.red_ryb), thickness = 4.dp, modifier = Modifier.width(10.dp))
-                }
-
-                Column(modifier = Modifier.padding(start = 10.dp)) {
-                    Text(
-                        text = movie.title,
-                        fontSize = 28.sp,
-                        style = MaterialTheme.typography.h2,
-                        color = MaterialTheme.colors.primary
-                    )
-
-                    Spacer(modifier = Modifier.height(5.dp))
-
-                    Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.spacedBy(10.dp)) {
+                item {
+                    Column(modifier = Modifier.padding(start = 10.dp)) {
                         Text(
-                            text = movie.year,
-                            style = MaterialTheme.typography.h6,
+                            text = movie.title,
+                            fontSize = 28.sp,
+                            style = MaterialTheme.typography.h2,
                             color = MaterialTheme.colors.primary
                         )
 
-                        Divider(
-                            Modifier
-                                .width(0.5.dp)
-                                .height(15.dp),
-                            thickness = 0.5.dp,
-                            color = MaterialTheme.colors.primary)
+                        Spacer(modifier = Modifier.height(5.dp))
 
-                        Card(
-                            modifier = Modifier.wrapContentSize(),
-                            shape = RoundedCornerShape(CornerSize(4.dp)),
-                            backgroundColor = colorResource(id = R.color.old_lavender)
-                        ) {
+                        Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.spacedBy(10.dp)) {
                             Text(
-                                text = movie.maturityRating,
-                                style = MaterialTheme.typography.h5,
-                                fontSize = 12.sp,
-                                color = MaterialTheme.colors.primary,
-                                modifier = Modifier.padding(start = 5.dp, end = 5.dp, top = 2.dp, bottom = 2.dp)
+                                text = movie.year,
+                                style = MaterialTheme.typography.h6,
+                                color = MaterialTheme.colors.primary
+                            )
+
+                            Divider(
+                                Modifier
+                                    .width(0.5.dp)
+                                    .height(15.dp),
+                                thickness = 0.5.dp,
+                                color = MaterialTheme.colors.primary)
+
+                            Card(
+                                modifier = Modifier.wrapContentSize(),
+                                shape = RoundedCornerShape(CornerSize(4.dp)),
+                                backgroundColor = colorResource(id = R.color.old_lavender)
+                            ) {
+                                Text(
+                                    text = movie.maturityRating,
+                                    style = MaterialTheme.typography.h5,
+                                    fontSize = 12.sp,
+                                    color = MaterialTheme.colors.primary,
+                                    modifier = Modifier.padding(start = 5.dp, end = 5.dp, top = 2.dp, bottom = 2.dp)
+                                )
+                            }
+
+                            Divider(
+                                Modifier
+                                    .width(0.5.dp)
+                                    .height(15.dp),
+                                thickness = 0.5.dp,
+                                color = MaterialTheme.colors.primary)
+
+                            Text(
+                                text = movie.time,
+                                style = MaterialTheme.typography.h6,
+                                color = MaterialTheme.colors.primary
                             )
                         }
 
-                        Divider(
-                            Modifier
-                                .width(0.5.dp)
-                                .height(15.dp),
-                            thickness = 0.5.dp,
-                            color = MaterialTheme.colors.primary)
-
-                        Text(
-                            text = movie.time,
-                            style = MaterialTheme.typography.h6,
-                            color = MaterialTheme.colors.primary
-                        )
                     }
-
                 }
 
+                item {
+                    Divider(
+                        thickness = 1.dp,
+                        modifier = Modifier.padding(
+                            start = 5.dp,
+                            end = 5.dp,
+                            top = 15.dp,
+                            bottom = 10.dp
+                        ),
+                        color = colorResource(id = R.color.gainsboro)
+                    )
+                }
+
+                item {
+                    Card(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(10.dp),
+                        backgroundColor = MaterialTheme.colors.background,
+                        shape = RoundedCornerShape(CornerSize(8.dp)),
+                        elevation = 4.dp
+                    ) {
+                        Column(modifier = Modifier.padding(5.dp), horizontalAlignment = Alignment.CenterHorizontally) {
+                            Column() {
+                                Text(text = movie.firstDescription, style = MaterialTheme.typography.body1, color = MaterialTheme.colors.primary)
+                                
+                                Spacer(modifier = Modifier.height(5.dp))
+                                
+                                AnimatedVisibility(visible = isReadMore) {
+                                    Column() {
+                                        Text(text = movie.secondDescription, style = MaterialTheme.typography.body1, color = MaterialTheme.colors.primary)
+
+                                        Spacer(modifier = Modifier.height(10.dp))
+
+                                        Row(verticalAlignment = Alignment.CenterVertically) {
+                                            Text(text = "Starring:", style = MaterialTheme.typography.h4, fontSize = 12.sp, color = MaterialTheme.colors.primary)
+                                            Spacer(modifier = Modifier.width(10.dp))
+                                            Text(text = movie.starring, style = MaterialTheme.typography.h6, color = MaterialTheme.colors.primary)
+                                        }
+
+                                        Row(verticalAlignment = Alignment.CenterVertically) {
+                                            Text(text = "Director:", style = MaterialTheme.typography.h4, fontSize = 12.sp, color = MaterialTheme.colors.primary)
+                                            Spacer(modifier = Modifier.width(10.dp))
+                                            Text(text = movie.director, style = MaterialTheme.typography.h6, color = MaterialTheme.colors.primary)
+                                        }
+                                    }
+                                }
+                            }
+                            
+                            Spacer(modifier = Modifier.height(10.dp))
+                            
+                            Row(modifier = Modifier.clickable {
+                                isReadMore = !isReadMore
+                            }) {
+
+                                val readMoreIcon = if (isReadMore) {
+                                    Icons.Default.KeyboardArrowUp
+                                } else {
+                                    Icons.Default.ArrowDropDown
+                                }
+
+                                Icon(
+                                    imageVector = readMoreIcon,
+                                    contentDescription = "Show More Button",
+                                    Modifier.size(20.dp),
+                                    tint = MaterialTheme.colors.primary
+                                )
+
+                                Spacer(modifier = Modifier.width(5.dp))
+
+                                Text(
+                                    text = "Read more",
+                                    style = MaterialTheme.typography.h6,
+                                    color = MaterialTheme.colors.primary
+                                )
+
+                            }
+
+                        }
+
+                    }
+                }
+
+                item {
+                    Spacer(modifier = Modifier.height(5.dp))
+
+                    Column(verticalArrangement = Arrangement.spacedBy(5.dp), modifier = Modifier.padding(5.dp)) {
+                        FilledRectangleButton(label = "Play", icon = Icons.Default.PlayArrow, onClick = {
+                            Toast.makeText(context, "Play ${movie.title}", Toast.LENGTH_SHORT).show()
+                        })
+
+                        val downloadIcon = if (isSystemInDarkTheme()) {
+                            painterResource(id = R.drawable.download_dark_theme_ic)
+                        } else {
+                            painterResource(id = R.drawable.download_light_theme_ic)
+                        }
+
+                        OutlinedRectangleButton(label = "Download", icon = downloadIcon, onClick = {
+                            Toast.makeText(context, "Downloading ${movie.title}", Toast.LENGTH_SHORT).show()
+                        })
+
+                    }
+                }
 
             }
         }
