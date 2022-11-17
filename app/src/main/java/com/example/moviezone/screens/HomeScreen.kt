b13d671ee10b8ea49471743eea7c3e2ed933a675
johnaeris <barrotjohnaeris@gmail.com>
2022-11-17 23:22:18 +0800

    Add movie category

--- app/src/main/java/com/example/moviezone/screens/HomeScreen.kt
+++ app/src/main/java/com/example/moviezone/screens/HomeScreen.kt
@@ -4,6 +4,7 @@ import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CornerSize
@@ -12,15 +13,17 @@ import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import coil.ImageLoader
-import coil.compose.AsyncImage
 import coil.compose.AsyncImagePainter
 import coil.compose.SubcomposeAsyncImage
 import coil.compose.SubcomposeAsyncImageContent
@@ -69,44 +72,139 @@ fun HomeScreen(navController: NavController) {
 
 @Composable
 fun MainContent(navController: NavController,
-                moviePosterList: List<MovieData> = getMovieData()
+                movieList: List<MovieData> = getMovieData()
 ) {
-    Column(
-        modifier = Modifier.padding(20.dp),
-        verticalArrangement = Arrangement.spacedBy(20.dp)
+    LazyColumn(
+        modifier = Modifier.padding(20.dp)
     ) {
+        item {
+            Column() {
+                Text(
+                    text = "Movies",
+                    style = MaterialTheme.typography.h2,
+                    color = MaterialTheme.colors.primary)
 
-        Column() {
-            Text(
-                text = "Movies",
-                style = MaterialTheme.typography.h2,
-                color = MaterialTheme.colors.primary)
+                Spacer(modifier = Modifier.height(5.dp))
 
-            Spacer(modifier = Modifier.height(5.dp))
+                Text(
+                    text = stringResource(R.string.movie_description),
+                    style = MaterialTheme.typography.body1,
+                    color = MaterialTheme.colors.primary)
 
-            Text(
-                text = stringResource(R.string.movie_description),
-                style = MaterialTheme.typography.body1,
-                color = MaterialTheme.colors.primary)
+                Spacer(modifier = Modifier.height(20.dp))
+            }
         }
 
-        Column() {
+        item {
+            Text(
+                text = "New Releases",
+                style = MaterialTheme.typography.h3,
+                color = colorResource(id = R.color.red_ryb))
+
+            LazyRow() {
+                items(items = movieList.filter { movie -> movie.genre.contains("New Release")}) { it ->
+                    NewMovieCard(movie = it) { movie ->
+                        navController.navigate(route = Screen.DetailScreen.name + "/$movie")
+                    }
+                }
+            }
+        }
+
+        item {
             Text(
                 text = "Anime",
                 style = MaterialTheme.typography.h3,
                 color = MaterialTheme.colors.primary)
 
             LazyRow() {
-                items(items = moviePosterList) {
+                items(items = movieList.filter { movie -> movie.genre.contains("Anime")}) { it ->
                     MovieCard(movie = it) { movie ->
                         navController.navigate(route = Screen.DetailScreen.name + "/$movie")
                     }
                 }
             }
         }
+
+        item {
+            Text(
+                text = "Adventure",
+                style = MaterialTheme.typography.h3,
+                color = MaterialTheme.colors.primary)
+
+            LazyRow() {
+                items(items = movieList.filter { movie -> movie.genre.contains("Adventure")}) { it ->
+                    MovieCard(movie = it) { movie ->
+                        navController.navigate(route = Screen.DetailScreen.name + "/$movie")
+                    }
+                }
+            }
+        }
+
+        item {
+            Text(
+                text = "Comedy",
+                style = MaterialTheme.typography.h3,
+                color = MaterialTheme.colors.primary)
+
+            LazyRow() {
+                items(items = movieList.filter { movie -> movie.genre.contains("Comedy")}) { it ->
+                    MovieCard(movie = it) { movie ->
+                        navController.navigate(route = Screen.DetailScreen.name + "/$movie")
+                    }
+                }
+            }
+        }
+
+        item {
+            Text(
+                text = "Action",
+                style = MaterialTheme.typography.h3,
+                color = MaterialTheme.colors.primary)
+
+            LazyRow() {
+                items(items = movieList.filter { movie -> movie.genre.contains("Action")}) { it ->
+                    MovieCard(movie = it) { movie ->
+                        navController.navigate(route = Screen.DetailScreen.name + "/$movie")
+                    }
+                }
+            }
+        }
+
+        item {
+            Text(
+                text = "Thriller",
+                style = MaterialTheme.typography.h3,
+                color = MaterialTheme.colors.primary)
+
+            LazyRow() {
+                items(items = movieList.filter { movie -> movie.genre.contains("Thriller")}) { it ->
+                    MovieCard(movie = it) { movie ->
+                        navController.navigate(route = Screen.DetailScreen.name + "/$movie")
+                    }
+                }
+            }
+        }
+
+        item {
+            Text(
+                text = "Drama",
+                style = MaterialTheme.typography.h3,
+                color = MaterialTheme.colors.primary)
+
+            LazyRow() {
+                items(items = movieList.filter { movie -> movie.genre.contains("Drama")}) { it ->
+                    MovieCard(movie = it) { movie ->
+                        navController.navigate(route = Screen.DetailScreen.name + "/$movie")
+                    }
+                }
+            }
+        }
+
+
     }
 }
 
+//Movie Poster Card
 @Composable
 private fun MovieCard(movie: MovieData, onPosterClick: (String) -> Unit) {
 
@@ -114,7 +212,8 @@ private fun MovieCard(movie: MovieData, onPosterClick: (String) -> Unit) {
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(5.dp)
+        verticalArrangement = Arrangement.spacedBy(5.dp),
+        modifier = Modifier.size(width = 100.dp, height = 200.dp)
     ) {
         Card(
             modifier = Modifier
@@ -148,6 +247,74 @@ private fun MovieCard(movie: MovieData, onPosterClick: (String) -> Unit) {
 
         Text(
             text = movie.title,
+            maxLines = 3,
+            textAlign = TextAlign.Center,
+            overflow = TextOverflow.Clip,
+            style = MaterialTheme.typography.caption,
+            color = MaterialTheme.colors.primary
+        )
+
+    }
+}
+
+//New Release Movie Poster Card
+@Composable
+private fun NewMovieCard(movie: MovieData, onPosterClick: (String) -> Unit) {
+
+    val context = LocalContext.current
+
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(5.dp),
+        modifier = Modifier.size(width = 100.dp, height = 200.dp)
+    ) {
+        Box() {
+            Card(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(start = 5.dp, end = 5.dp)
+                    .size(100.dp, 140.dp)
+                    .clickable {
+                        Toast
+                            .makeText(context, movie.title, Toast.LENGTH_SHORT)
+                            .show()
+                        onPosterClick(movie.id)
+                    },
+                backgroundColor = MaterialTheme.colors.primaryVariant,
+                shape = RoundedCornerShape(corner = CornerSize(4.dp)),
+                elevation = 8.dp)
+            {
+
+                SubcomposeAsyncImage(
+                    model = movie.poster,
+                    contentDescription = "{${movie.title} Poster}",
+                    contentScale = ContentScale.FillBounds
+                ) {
+                    val state = painter.state
+                    if (state is AsyncImagePainter.State.Loading || state is AsyncImagePainter.State.Error) {
+                        CircularProgressIndicator()
+                    } else {
+                        SubcomposeAsyncImageContent()
+                    }
+                }
+            }
+
+            Card(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .padding(start = 10.dp, top = 5.dp),
+                backgroundColor = colorResource(id = R.color.red_ryb),
+                shape = RoundedCornerShape(CornerSize(2.dp))
+            ) {
+                Text(text = "New", style = MaterialTheme.typography.h1, fontSize = 8.sp, color = Color.White, modifier = Modifier.padding(bottom = 2.dp, start = 3.dp, end = 3.dp))
+            }
+        }
+
+        Text(
+            text = movie.title,
+            maxLines = 3,
+            textAlign = TextAlign.Center,
+            overflow = TextOverflow.Clip,
             style = MaterialTheme.typography.caption,
             color = MaterialTheme.colors.primary
         )
